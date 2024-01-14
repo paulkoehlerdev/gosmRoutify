c155dc57cf7226bd8751af3b5c2edc7559454e60
Paul Köhler <koehler_paul@outlook.de>
2024-01-14 11:59:46 +0100

    Fix marker icon on prod

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -6,6 +6,16 @@ import { createApp } from 'vue'
 
 import App from './App.vue'
 import mitt from 'mitt'
+import L from 'leaflet'
+
+// eslint-disable-next-line
+delete L.Icon.Default.prototype._getIconUrl
+// eslint-disable-next-line
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: import('leaflet/dist/images/marker-icon-2x.png'),
+  iconUrl: import('leaflet/dist/images/marker-icon.png'),
+  shadowUrl: import('leaflet/dist/images/marker-shadow.png')
+})
 
 const app = createApp(App)
 
